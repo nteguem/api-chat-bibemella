e56262bae78f5813161cf5040453bf53168f2e57
Nteguem roland <nteguemroland@gmail.com>
2024-03-31 07:17:48 +0100

    review session save

--- api/helpers/whatsApp/whatappsHandler.js
+++ api/helpers/whatsApp/whatappsHandler.js
@@ -7,11 +7,11 @@ const initializeWhatsAppClient = (io) => {
   const client = new Client({
     puppeteer: {
       args: ['--no-sandbox'],
-      executablePath: '/usr/bin/google-chrome-stable',
+      // executablePath: '/usr/bin/google-chrome-stable',
     },
     authStrategy: new LocalAuth(
       {
-        dataPath: '../sessions'
+        dataPath: '../sessions/session'
       }
     ),
   });
