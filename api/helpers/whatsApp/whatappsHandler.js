6ada5614c6d9d79a63ebcc47fd1027bc2b8c78ea
Nteguem roland <nteguemroland@gmail.com>
2024-03-31 07:21:53 +0100

    review

--- api/helpers/whatsApp/whatappsHandler.js
+++ api/helpers/whatsApp/whatappsHandler.js
@@ -11,7 +11,7 @@ const initializeWhatsAppClient = (io) => {
     },
     authStrategy: new LocalAuth(
       {
-        dataPath: '../sessions/session'
+        dataPath: '../sessions'
       }
     ),
   });
